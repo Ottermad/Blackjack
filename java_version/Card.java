0ce7f52930f75892833a6758238317c6726571fc
Ottermad <charlie.thomas@attwoodthomas.net>
2015-05-20 20:00:32 +0100

    Started at Card.java

---
+++ java_version/Card.java
@@ -0,0 +1,26 @@
+public class Card {
+    public static String[] suits = {
+        "hearts",
+        "diamonds",
+        "spades",
+        "clubs"
+    }
+    
+    public static String[] names = {
+        "ace",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "jack",
+        "queen",
+        "king"
+    }
+
+    public static
+}
